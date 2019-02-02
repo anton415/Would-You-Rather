fff7f89f4cb6be61f6060eab1a63e34e4c50b46d
Anton Serdyuchenko <anton415@gmail.com>
2019-02-02 18:28:13 +0300

    Create simple login form in app.js page.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Home
+          New Question
+          Leaders
+          Board
         </header>
+        <main className="App-main">
+          <p>Welcom to the Would You Rather App!</p>
+          <p>Please sign in to continue</p>
+
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <form>
+            <p>Sign in</p>
+            <select>
+              <option value="tyler">Tyler</option>
+              <option value="anton">Anton</option>
+            </select>
+            <Button variant="contained" color="primary">Sign in</Button>
+          </form>
+        </main>
       </div>
     );
   }
