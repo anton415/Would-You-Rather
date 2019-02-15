49828a0613f2f030f455116da723c31b5a3ac656
Anton Serdyuchenko <anton415@gmail.com>
2019-02-15 05:45:44 +0300

    Edit helpers. Delete author's name and url.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,5 +1,17 @@
-export function formatQuestion () {
+export function formatQuestion (question, author, authedUser) {
+  const { id, optionOneText, optionTwoText, timestamp } = question
+
   return {
-    
+    id,
+    timestamp,
+    author,
+    optionOne: {
+      votes: [],
+      text: optionOneText,
+    },
+    optionTwo: {
+      votes: [],
+      text: optionTwoText,
+    }
   }
 }
