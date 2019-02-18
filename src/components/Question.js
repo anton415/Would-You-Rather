8b72ab324a655afe1bc98d06d3a95411032f3d55
Anton Serdyuchenko <anton415@gmail.com>
2019-02-18 06:09:21 +0300

    Add user data in Question component. Add get his avatarURL.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { formatQuestion } from '../utils/helpers'
+import { formatQuestion, formatUser } from '../utils/helpers'
 
 class Question extends Component {
   render() {
     const { question } = this.props
+    const { userID } = this.props
+    const { users } = this.props
 
     if (question === null) {
       return <p>This Question doesn't existed</p>
+    }
+    if (userID === null) {
+      return <p>This User doesn't existed</p>
     }
 
     const {
       id, author, timestamp, optionOne, optionTwo
     } = question
 
-    // console.log(this.props)
+    // const {
+    //   userID, name, avatarURL, answers, questions
+    // } = user
+    var myUser = ''
+    console.log(this.props)
+    console.log('User id: ', userID)
+    console.log('Users: ', users)
+    console.log('User id in all users: ', users[userID])
+    if (typeof users[userID] !== 'undefined') {
+      myUser =  users[userID]
+      console.log("My user: ", myUser)
+      const nameOfUser = myUser['name']
+      console.log("My user's name: ", nameOfUser)
+    }
+
 
     return (
       <div className='question'>
         <img
-          src={author}
+          src={myUser.avatarURL}
           alt={`Avatar of ${author}`}
           className='avatar'
         />
@@ -30,12 +49,15 @@ class Question extends Component {
 
 function mapStateToProps ({authedUser, users, questions}, { id }) {
   const question = questions[id]
+  const userID = questions[id].author
 
   return {
     authedUser,
     question: question
       ? formatQuestion(question, users[question.author], authedUser)
-      : null
+      : null,
+    userID,
+    users
   }
 }
 
