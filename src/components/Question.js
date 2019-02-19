5db5e36e404f773127fdb92ff8169edcaf5de234
Anton Serdyuchenko <anton415@gmail.com>
2019-02-19 05:55:26 +0300

    Add code more beauty. Add css file.

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { formatQuestion, formatUser } from '../utils/helpers'
+import { formatQuestion } from '../utils/helpers'
+import './Question.css';
 
 class Question extends Component {
   render() {
@@ -11,37 +12,32 @@ class Question extends Component {
     if (question === null) {
       return <p>This Question doesn't existed</p>
     }
-    if (userID === null) {
-      return <p>This User doesn't existed</p>
-    }
 
     const {
       id, author, timestamp, optionOne, optionTwo
     } = question
 
-    // const {
-    //   userID, name, avatarURL, answers, questions
-    // } = user
-    var myUser = ''
-    console.log(this.props)
-    console.log('User id: ', userID)
-    console.log('Users: ', users)
-    console.log('User id in all users: ', users[userID])
+    let myUser = ''
     if (typeof users[userID] !== 'undefined') {
       myUser =  users[userID]
-      console.log("My user: ", myUser)
-      const nameOfUser = myUser['name']
-      console.log("My user's name: ", nameOfUser)
     }
 
 
     return (
       <div className='question'>
+        <p>{myUser.name}</p>
         <img
           src={myUser.avatarURL}
           alt={`Avatar of ${author}`}
           className='avatar'
         />
+        <div>
+          <p>Would you rather</p>
+          <div>
+            {optionOne.text} or {optionTwo.text}
+          </div>
+          <button>View Poll</button>
+        </div>
       </div>
     )
   }
