28ec2726f3692a82dffb728a1d890f53c08c1cda
Anton Serdyuchenko <anton415@gmail.com>
2019-02-24 17:41:09 +0300

    Display only view poll page.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
 import './App.css';
-import Button from '@material-ui/core/Button';
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
 import LoadingBar from 'react-redux-loading'
+import ViewPoll from './ViewPoll'
 
 class App extends Component {
   componentDidMount() {
@@ -16,35 +15,9 @@ class App extends Component {
     return (
       <div className="App">
         <LoadingBar />
-        <header className="App-header">
-          Home
-          New Question
-          Leaders
-          Board
-        </header>
-        <main className="App-main">
-          <p>Welcom to the Would You Rather App!</p>
-          <p>Please sign in to continue</p>
-
-          <img src={logo} className="App-logo" alt="logo" />
-
-          <form>
-            <p>Sign in</p>
-            <div className="select">
-            <select>
-              <option value="">Select user</option>
-              <option value="tyler">Tyler</option>
-              <option value="anton">Anton</option>
-            </select>
-            </div>
-
-            <Button variant="contained" color="primary">Sign in</Button>
-          </form>
-
-          {this.props.loading === true
-            ? null
-            : <Dashboard />}
-        </main>
+        {this.props.loading === true
+          ? null
+          : <ViewPoll />}
       </div>
     );
   }
