794b78c9cef848b0c1dadfe2d2c1ea2859a07f67
Anton Serdyuchenko <anton415@gmail.com>
2019-03-10 09:45:21 +0300

    Add 'add question' in action.

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,9 +1,17 @@
 export const GET_QUESTIONS = 'GET_QUESTIONS'
+export const ADD_QUESTION = 'ADD_QUESTION'
 
 export function getQuestions(questions) {
   return {
     type: GET_QUESTIONS,
     questions
+  }
+}
+
+export function addQuestion(question) {
+  return {
+    type: ADD_QUESTION,
+    question
   }
 }
 
