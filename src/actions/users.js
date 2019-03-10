686fedaa107827b0638b959e7923eec47ab41187
Anton Serdyuchenko <anton415@gmail.com>
2019-03-10 11:35:31 +0300

    Add 'add answer to user'.

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,5 +1,6 @@
 export const RECEIVE_USERS = 'RECEIVE_USERS'
 export const ADD_QUESTION_TO_USER = 'ADD_QUESTION_TO_USER'
+export const ADD_ANSWER_TO_USER = 'ADD_ANSWER_TO_USER'
 
 export function recieveUsers (users) {
   return {
@@ -18,5 +19,20 @@ function addQuestionToUser(question) {
 export function handleAddQuestionToUser(info) {
   return (dispatch) => {
     dispatch(addQuestionToUser(info))
+  }
+}
+
+function addAnswerToUser(authedUser, questionId, answer) {
+  return {
+    type: ADD_ANSWER_TO_USER,
+    authedUser,
+    questionId,
+    answer
+  }
+}
+
+export function handleAddAnswerToUser(authedUser, questionId, answer) {
+  return (dispatch) => {
+    dispatch(addAnswerToUser(authedUser, questionId, answer))
   }
 }
